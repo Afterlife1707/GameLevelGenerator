337c812f6ca1b65f64e16b29352fb976d0f36ad7
Hritick Buragohain <bestfighter17@gmail.com>
2024-04-12 21:36:06 +0100

    Update run_keras_server.py

--- run_keras_server.py
+++ run_keras_server.py
@@ -3,6 +3,7 @@ import flask
 import numpy as np
 import tensorflow as tf
 import keras
+from keras import backend as K
 
 print("TensorFlow version:", tf.__version__)
 print("Keras version:", keras.__version__)
@@ -20,34 +21,62 @@ app = flask.Flask(__name__)
 model_path = 'VAE_Room_Generator_Decoder_1352.h5'
 model = None
 
+img_width = 16
+img_height = 16
+num_channels = 1
+input_shape = (img_height, img_width, num_channels)
+
+latent_dim = 150  # Number of latent dim parameters
+
+input_img = tf.keras.layers.Input(shape=input_shape, name='encoder_input')
+x = tf.keras.layers.Conv2D(filters=128, kernel_size=3, strides=(2, 2), padding='same', activation='relu')(input_img)
+x = tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding='same', activation='relu')(x)
+x = tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding='same', activation='relu')(x)
+x = tf.keras.layers.Conv2D(filters=16, kernel_size=3, strides=(2, 2), padding='same', activation='relu')(x)
+
+conv_shape = tf.keras.backend.int_shape(x)  # Shape of conv to be provided to decoder
+x = tf.keras.layers.Flatten()(x)
+x = tf.keras.layers.Dense(20, activation='relu')(x)
+
+z_mu = tf.keras.layers.Dense(latent_dim, name='latent_mu')(x)  # Mean values of encoded input
+z_sigma = tf.keras.layers.Dense(latent_dim, name='latent_sigma')(x)
+
+class CustomLayer(keras.layers.Layer):
+
+    def vae_loss(self, x, z_decoded):
+        x = K.flatten(x)
+        z_decoded = K.flatten(z_decoded)
+
+        # Reconstruction loss (as we used sigmoid activation we can use binarycrossentropy)
+        recon_loss = keras.metrics.binary_crossentropy(x, z_decoded) * img_width * img_height
+
+        # KL divergence
+        #kl_loss = -5e-4 * K.mean(1 + z_sigma - K.square(z_mu) - K.exp(z_sigma), axis=-1)
+
+        # KL divergence loss
+        kl_loss = 1 + z_sigma - K.square(z_mu) - K.exp(z_sigma)
+        kl_loss = K.sum(kl_loss, axis=-1)
+        kl_loss *= -.5
+
+        return K.mean(recon_loss + (.75 * kl_loss))
+
+    # add custom loss to the class
+    def call(self, inputs):
+        x = inputs[0]
+        z_decoded = inputs[1]
+        loss = self.vae_loss(x, z_decoded)
+        self.add_loss(loss, inputs=inputs)
+        return x
+
 def load_model():
     global model
     model = tf.keras.models.load_model(model_path)
+    decoder = keras.saving.load_model(model_path, custom_objects={'CustomLayer': CustomLayer()}, safe_mode=False, compile=True)
 
 def generate_image():
     # Load the model
     if model is None:
         load_model()
-
-    img_width = 16
-    img_height = 16
-    num_channels = 1
-    input_shape = (img_height, img_width, num_channels)
-
-    latent_dim = 150  # Number of latent dim parameters
-
-    input_img = tf.keras.layers.Input(shape=input_shape, name='encoder_input')
-    x = tf.keras.layers.Conv2D(filters=128, kernel_size=3, strides=(2, 2), padding='same', activation='relu')(input_img)
-    x = tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding='same', activation='relu')(x)
-    x = tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding='same', activation='relu')(x)
-    x = tf.keras.layers.Conv2D(filters=16, kernel_size=3, strides=(2, 2), padding='same', activation='relu')(x)
-
-    conv_shape = tf.keras.backend.int_shape(x)  # Shape of conv to be provided to decoder
-    x = tf.keras.layers.Flatten()(x)
-    x = tf.keras.layers.Dense(20, activation='relu')(x)
-
-    z_mu = tf.keras.layers.Dense(latent_dim, name='latent_mu')(x)  # Mean values of encoded input
-    z_sigma = tf.keras.layers.Dense(latent_dim, name='latent_sigma')(x)
 
     vector = np.random.normal(0, 1, size=(1, 50))
     X = model.predict(vector)
