c516027461c98c1608e7f3cc00294df9d04da47f
Hritick Buragohain <bestfighter17@gmail.com>
2024-04-12 02:05:05 +0100

    reintroduced conv layers

--- run_keras_server.py
+++ run_keras_server.py
@@ -1,11 +1,13 @@
 import os
 import flask
 import numpy as np
-from numpy import asarray
 import tensorflow as tf
 
 # Set CPU as available physical device
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
+# Disable eager execution
+tf.compat.v1.disable_eager_execution()
 
 # Initialize Flask application
 app = flask.Flask(__name__)
@@ -13,32 +15,49 @@ app = flask.Flask(__name__)
 # Load the pre-trained Keras model
 model_path = 'VAE_Room_Generator_Decoder_1352.h5'
 model = tf.keras.models.load_model(model_path)
-latent_dim = 150
 
 def generate_image():
-    # Generate a random vector
-    vector = np.random.normal(0, 1, size=(1, latent_dim))
-    # Generate image using the model
+    img_width = 16
+    img_height = 16
+    num_channels = 1
+    input_shape = (img_height, img_width, num_channels)
+
+    latent_dim = 150  # Number of latent dim parameters
+
+    input_img = tf.keras.layers.Input(shape=input_shape, name='encoder_input')
+    x = tf.keras.layers.Conv2D(filters=128, kernel_size=3, strides=(2, 2), padding='same', activation='relu')(input_img)
+    x = tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding='same', activation='relu')(x)
+    x = tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding='same', activation='relu')(x)
+    x = tf.keras.layers.Conv2D(filters=16, kernel_size=3, strides=(2, 2), padding='same', activation='relu')(x)
+
+    conv_shape = tf.keras.backend.int_shape(x)  # Shape of conv to be provided to decoder
+    x = tf.keras.layers.Flatten()(x)
+    x = tf.keras.layers.Dense(20, activation='relu')(x)
+
+    z_mu = tf.keras.layers.Dense(latent_dim, name='latent_mu')(x)  # Mean values of encoded input
+    z_sigma = tf.keras.layers.Dense(latent_dim, name='latent_sigma')(x)  # Std dev. (variance) of encoded input
+
+    vector = np.random.normal(0, 1, size=(1, 50))
     X = model.predict(vector)
+
     return X
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    # Initialize the data dictionary
     data = {"success": False}
 
-    # Ensure an image was properly uploaded
     if flask.request.method == "POST":
-        # Generate the image
         image = generate_image()
-        # Prepare the image for response
-        results = asarray(image)
+        print("Image has been generated")
+
+        results = np.asarray(image)
         data["results"] = results.tolist()
+
         data["success"] = True
 
-    # Return the data dictionary as JSON response
     return flask.jsonify(data)
 
 if __name__ == "__main__":
-    print("Loading Keras model and starting Flask server...")
+    print(("* Loading Keras model and Flask starting server..."
+        "please wait until server has fully started"))
     app.run()
